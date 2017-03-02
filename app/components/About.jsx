6bac9679f18e1f4bce7c6331b654e2183fc0bd30
Dmitriy Chervan <dmitriy.chervan@yandex.ru>
2017-03-02 21:22:57 +0300

    Style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const About = () => (
-  <h3>About</h3>
+  <div>
+    <h3 className="text-center">About</h3>
+    <p>This is a weather application build on React</p>
+    <p>Here are some of the tools I used:</p>
+    <ul>
+      <li>React</li>
+      <li>Open Weather Map</li>
+    </ul>
+  </div>
 );
 
 export default About;
